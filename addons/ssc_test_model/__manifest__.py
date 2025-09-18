93292ac238ff210175d3cf173f511a9d71b0f1b6
IbrahimMZnad <id7sp22@gmail.com>
2025-09-18 14:22:29 +0400

    Initial clean commit

---
+++ addons/ssc_test_model/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    'name': 'SSC Test Model',
+    'version': '1.0',
+    'summary': 'Simple test model for SSC',
+    'description': 'This is a test module to verify menu and dashboard visibility.',
+    'author': 'Ibrahim Alznad',
+    'category': 'Tools',
+    'depends': ['base'],
+    'data': [
+        'views/test_model_views.xml',
+        'views/test_model_action.xml',
+    ],
+    'application': True,
+    'installable': True,
+    'auto_install': False,
+}
