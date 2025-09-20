4ccd1fbef9110011955905fb3c97a30a3e590f39
IbrahimMZnad <id7sp22@gmail.com>
2025-09-20 10:47:55 +0400

    Add form and list menus to ssc_test_model

--- addons/ssc_test_model/models/test_model.py
+++ addons/ssc_test_model/models/test_model.py
@@ -4,5 +4,6 @@ class SscTestSimple(models.Model):
     _name = "ssc.test.simple"
     _description = "Simple Test Menu"
 
+    name = fields.Char(string="Name", required=True)  # إضافة حقل name
     date_field = fields.Date(string="Date")
     is_checked = fields.Boolean(string="Checked")
