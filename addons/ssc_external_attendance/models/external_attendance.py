e9bd0cab53345bd2b7c68e195a9a612d3d80cdd9
IbrahimMZnad <id7sp22@gmail.com>
2025-09-20 14:10:17 +0400

    update ssc_test_model simple menu

--- addons/ssc_external_attendance/models/external_attendance.py
+++ addons/ssc_external_attendance/models/external_attendance.py
@@ -1,23 +1,30 @@
 from odoo import models, fields, api
-from datetime import datetime
+from datetime import date
 
 class ExternalAttendance(models.Model):
-    _name = 'ssc.external.attendance'
-    _description = 'External Attendance'
+    _name = "ssc.external.attendance"
+    _description = "External Attendance"
 
-    name = fields.Char(string="Name", default=lambda self: "Today's Date (%s)" % fields.Date.today())
-    date_field = fields.Date(string="Date", default=fields.Date.today)
+    name = fields.Char(string="Name", default=lambda self: f"Today's Date ({date.today()})")
+    date_field = fields.Date(string="Date", default=fields.Date.context_today)
     day_type = fields.Selection(
-        [('regular', 'Regular Day'), ('off', 'Off Day')],
-        string="Type",
-        default=lambda self: 'off' if datetime.today().strftime('%A') == 'Friday' else 'regular'
+        [
+            ("regular", "Regular Day"),
+            ("off", "Off Day"),
+        ],
+        string="Day Type",
+        default="regular"
     )
 
-    attendance_sheet = fields.One2many('ssc.external.attendance.line', 'attendance_id', string="Attendance Sheet")
+    # One2many to attendance lines
+    line_ids = fields.One2many("ssc.external.attendance.line", "attendance_ref", string="Attendance Lines")
+
 
 class ExternalAttendanceLine(models.Model):
-    _name = 'ssc.external.attendance.line'
-    _description = 'External Attendance Line'
+    _name = "ssc.external.attendance.line"
+    _description = "External Attendance Line"
 
-    attendance_id = fields.Many2one('ssc.external.attendance', string="Attendance")
-    name = fields.Char(string="Attendance ID")
+    name = fields.Char(string="Name")
+    attendance_id = fields.Char(string="Attendance ID")
+
+    attendance_ref = fields.Many2one("ssc.external.attendance", string="Attendance Reference")
