34d434372ff7820128048253514ba077e087d9f3
IbrahimMZnad <id7sp22@gmail.com>
2025-09-20 12:52:24 +0400

    update ssc_test_model simple menu

--- addons/ssc_external_attendance/models/external_attendance.py
+++ addons/ssc_external_attendance/models/external_attendance.py
@@ -1,33 +1,30 @@
-from odoo import models, fields, api
+# -*- coding: utf-8 -*-
+from odoo import models, fields
 from datetime import date
 
-class SscExternalAttendance(models.Model):
-    _name = "ssc.external.attendance"
-    _description = "External Attendance"
+class ExternalAttendance(models.Model):
+    _name = 'ssc.external.attendance'
+    _description = 'External Attendance'
 
-    name = fields.Char(string="Name", default=lambda self: str(date.today()))
-    date_field = fields.Date(string="Date", default=fields.Date.today)
-    day_type = fields.Selection([
-        ('regular', 'Regular Day'),
-        ('off', 'Off Day')
-    ], string="Day Type", default='regular')
+    name = fields.Char(string='Name')
+    date_field = fields.Date(string='Date', default=fields.Date.context_today)
+    day_type = fields.Selection(
+        [('Regular Day', 'Regular Day'), ('Off Day', 'Off Day')],
+        string='Day Type',
+        default=lambda self: 'Off Day' if date.today().weekday() == 4 else 'Regular Day'
+    )
 
-    line_one_ids = fields.One2many('ssc.external.attendance.line', 'attendance_id_main', string="Line One")
-    line_two_ids = fields.One2many('ssc.external.attendance.line', 'attendance_id_main', string="Line Two")
+    att_record = fields.One2many(
+        'ssc.external.attendance.line', 
+        'parent_id', 
+        string='Attendance Lines'
+    )
 
-    @api.model
-    def default_get(self, fields_list):
-        res = super().default_get(fields_list)
-        if date.today().weekday() == 4:  # Friday
-            res['day_type'] = 'off'
-        else:
-            res['day_type'] = 'regular'
-        return res
 
-class SscExternalAttendanceLine(models.Model):
-    _name = "ssc.external.attendance.line"
-    _description = "External Attendance Line"
+class ExternalAttendanceLine(models.Model):
+    _name = 'ssc.external.attendance.line'
+    _description = 'Attendance Line'
 
-    name = fields.Char(string="Name")
-    attendance_id = fields.Char(string="Attendance ID")
-    attendance_id_main = fields.Many2one('ssc.external.attendance', string="Attendance Reference")
+    name = fields.Char(string='Name')
+    attendance_id = fields.Char(string='Attendance ID')
+    parent_id = fields.Many2one('ssc.external.attendance', string='Parent Attendance')
