0ddd80ad01e8536e4f0bd923f2bea72807b438a8
IbrahimMZnad <id7sp22@gmail.com>
2025-09-20 13:33:04 +0400

    update ssc_test_model simple menu

--- addons/ssc_external_attendance/models/external_attendance.py
+++ addons/ssc_external_attendance/models/external_attendance.py
@@ -7,25 +7,27 @@ class ExternalAttendanceLine(models.Model):
 
     name = fields.Char(string="Name")
     attendance_id = fields.Char(string="Attendance ID")
-    external_attendance_id = fields.Many2one('ssc.external.attendance', string="Attendance Reference")
+    attendance_ref = fields.Many2one('ssc.external.attendance', string="Attendance Reference")
+
 
 class ExternalAttendance(models.Model):
     _name = 'ssc.external.attendance'
     _description = 'External Attendance'
 
     name = fields.Char(string="Name")
-    date_field = fields.Date(string="Date", default=date.today())
-    day_type = fields.Selection([('regular','Regular Day'),('off','Off Day')], string="Day Type", default='regular')
-    att_record = fields.One2many('ssc.external.attendance.line', 'external_attendance_id', string="Attendance Records")
+    date_field = fields.Date(string="Date", default=date.today)
+    day_type = fields.Selection(
+        [('regular', 'Regular Day'), ('off', 'Off Day')],
+        string="Day Type",
+        default='regular'
+    )
+    att_record = fields.One2many('ssc.external.attendance.line', 'attendance_ref', string="Attendance Records")
 
     @api.model
     def create(self, vals):
-        if 'date_field' in vals and vals['date_field']:
-            dt = vals['date_field']
-        else:
-            dt = date.today()
-        if dt.weekday() == 4:  # الجمعة
-            vals['day_type'] = 'off'
-        else:
-            vals['day_type'] = 'regular'
+        # لو التاريخ جمعة -> Off Day
+        dt = vals.get('date_field') or date.today()
+        if isinstance(dt, str):
+            dt = fields.Date.from_string(dt)
+        vals['day_type'] = 'off' if dt.weekday() == 4 else 'regular'
         return super().create(vals)
