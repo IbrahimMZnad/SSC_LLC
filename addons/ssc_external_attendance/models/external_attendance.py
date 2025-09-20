375f8df9baf6b5bb0238be4e7036017afe6b9d41
IbrahimMZnad <id7sp22@gmail.com>
2025-09-20 14:02:14 +0400

    update ssc_test_model simple menu

--- addons/ssc_external_attendance/models/external_attendance.py
+++ addons/ssc_external_attendance/models/external_attendance.py
@@ -1,25 +1,23 @@
-from odoo import models, fields
+from odoo import models, fields, api
+from datetime import datetime
 
 class ExternalAttendance(models.Model):
     _name = 'ssc.external.attendance'
     _description = 'External Attendance'
 
-    name = fields.Char(string="Reference")
-    date = fields.Date(string="Date")
-    note = fields.Text(string="Note")
-
-    line_ids = fields.One2many(
-        'ssc.external.attendance.line',
-        'attendance_id',
-        string="Lines"
+    name = fields.Char(string="Name", default=lambda self: "Today's Date (%s)" % fields.Date.today())
+    date_field = fields.Date(string="Date", default=fields.Date.today)
+    day_type = fields.Selection(
+        [('regular', 'Regular Day'), ('off', 'Off Day')],
+        string="Type",
+        default=lambda self: 'off' if datetime.today().strftime('%A') == 'Friday' else 'regular'
     )
 
+    attendance_sheet = fields.One2many('ssc.external.attendance.line', 'attendance_id', string="Attendance Sheet")
 
 class ExternalAttendanceLine(models.Model):
     _name = 'ssc.external.attendance.line'
     _description = 'External Attendance Line'
 
-    attendance_id = fields.Many2one(
-        'ssc.external.attendance',
-        string="Attendance"
-    )
+    attendance_id = fields.Many2one('ssc.external.attendance', string="Attendance")
+    name = fields.Char(string="Attendance ID")
