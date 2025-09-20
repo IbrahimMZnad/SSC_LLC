b7e40776387060f7922ac49fcab7ce7932d68f06
IbrahimMZnad <id7sp22@gmail.com>
2025-09-20 13:00:13 +0400

    update ssc_test_model simple menu

--- addons/ssc_external_attendance/models/external_attendance.py
+++ addons/ssc_external_attendance/models/external_attendance.py
@@ -1,30 +1,31 @@
-# -*- coding: utf-8 -*-
-from odoo import models, fields
+from odoo import models, fields, api
 from datetime import date
+
+class ExternalAttendanceLine(models.Model):
+    _name = 'ssc.external.attendance.line'
+    _description = 'External Attendance Line'
+
+    name = fields.Char(string="Name")
+    attendance_id = fields.Char(string="Attendance ID")
+    external_attendance_id = fields.Many2one('ssc.external.attendance', string="Attendance Reference")
 
 class ExternalAttendance(models.Model):
     _name = 'ssc.external.attendance'
     _description = 'External Attendance'
 
-    name = fields.Char(string='Name')
-    date_field = fields.Date(string='Date', default=fields.Date.context_today)
-    day_type = fields.Selection(
-        [('Regular Day', 'Regular Day'), ('Off Day', 'Off Day')],
-        string='Day Type',
-        default=lambda self: 'Off Day' if date.today().weekday() == 4 else 'Regular Day'
-    )
+    name = fields.Char(string="Name")
+    date_field = fields.Date(string="Date", default=date.today())
+    day_type = fields.Selection([('regular','Regular Day'),('off','Off Day')], string="Day Type", default='regular')
+    att_record = fields.One2many('ssc.external.attendance.line', 'external_attendance_id', string="Attendance Records")
 
-    att_record = fields.One2many(
-        'ssc.external.attendance.line', 
-        'parent_id', 
-        string='Attendance Lines'
-    )
-
-
-class ExternalAttendanceLine(models.Model):
-    _name = 'ssc.external.attendance.line'
-    _description = 'Attendance Line'
-
-    name = fields.Char(string='Name')
-    attendance_id = fields.Char(string='Attendance ID')
-    parent_id = fields.Many2one('ssc.external.attendance', string='Parent Attendance')
+    @api.model
+    def create(self, vals):
+        if 'date_field' in vals and vals['date_field']:
+            dt = vals['date_field']
+        else:
+            dt = date.today()
+        if dt.weekday() == 4:  # الجمعة
+            vals['day_type'] = 'off'
+        else:
+            vals['day_type'] = 'regular'
+        return super().create(vals)
