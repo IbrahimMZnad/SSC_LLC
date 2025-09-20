1fcb5ad2449e0b8865f7bf6f8b83c5d47ec5e693
IbrahimMZnad <id7sp22@gmail.com>
2025-09-20 14:31:53 +0400

    update ssc_test_model simple menu

--- addons/ssc_external_attendance/models/external_attendance.py
+++ addons/ssc_external_attendance/models/external_attendance.py
@@ -1,31 +1,23 @@
-from odoo import models, fields
+from odoo import models, fields, api
 from datetime import date
 
 class ExternalAttendance(models.Model):
     _name = "ssc.external.attendance"
     _description = "External Attendance"
 
-    name = fields.Char(string="Name", default=lambda self: f"Today's Date ({date.today()})")
-    date_field = fields.Date(string="Date", default=fields.Date.context_today)
-    day_type = fields.Selection(
-        [
-            ("regular", "Regular Day"),
-            ("off", "Off Day"),
-        ],
-        string="Day Type",
-        default="regular"
+    name = fields.Char(string="Name", required=True, default=lambda self: str(date.today()))
+    date = fields.Date(string="Date", default=fields.Date.context_today)
+    type = fields.Selection(
+        [('Regular Day', 'Regular Day'), ('Off Day', 'Off Day')],
+        string="Type",
+        default=lambda self: 'Off Day' if date.today().weekday() == 4 else 'Regular Day'  # الجمعة = 4
     )
-
-    line_ids = fields.One2many(
-        "ssc.external.attendance.line",  # موديل الـ lines
-        "dummy_id",                       # field dummy فقط لتجنب FK
-        string="Attendance Lines"
-    )
+    line_ids = fields.One2many('ssc.external.attendance.line', 'external_id', string="Attendance Lines")
 
 
 class ExternalAttendanceLine(models.Model):
     _name = "ssc.external.attendance.line"
     _description = "External Attendance Line"
 
+    external_id = fields.Many2one('ssc.external.attendance', string="Attendance Reference", ondelete='cascade')
     attendance_id = fields.Char(string="Attendance ID")
-    dummy_id = fields.Char()  # هذا موجود فقط لتفادي FK
