f74afa5f33067c84c04e104223a698a60ee02c49
IbrahimMZnad <id7sp22@gmail.com>
2025-09-20 13:00:13 +0400

    update ssc_test_model simple menu

--- addons/ssc_external_attendance/__manifest__.py
+++ addons/ssc_external_attendance/__manifest__.py
@@ -1,15 +1,13 @@
 {
-    'name': 'SSC External Attendance',
+    'name': 'External Attendance',
     'version': '1.0',
-    'summary': 'External Attendance Module',
-    'description': 'A simple module for external attendance records',
-    'author': 'Ibrahim Alznad',
     'category': 'Human Resources',
+    'summary': 'Simple external attendance tracking',
+    'author': 'Ibrahim Alznad',
     'depends': ['base'],
     'data': [
         'views/external_attendance_views.xml',
     ],
-    'application': True,
     'installable': True,
-    'auto_install': False,
+    'application': True,
 }
