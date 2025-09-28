57a3c0e0504685e3e4df57f97dee4ad47fefb236
IbrahimMZnad <id7sp22@gmail.com>
2025-09-28 13:07:33 +0400

    Add project_consumption module with form and tree views

---
+++ addons/project_consumption/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    'name': 'Project Consumption',
+    'version': '1.0',
+    'summary': 'Track consumed materials per project',
+    'description': 'This module tracks project materials consumption using x_all_items_list.',
+    'author': 'Ibrahim Alznad',
+    'category': 'Project',
+    'depends': ['base', 'project', 'x_all_items_list'],  # الموديول الجاهز اللي بدنا نستخدمه
+    'data': [
+        'views/project_consumption_views.xml',
+    ],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+}
--- addons/ssc_test_model/models/test_model.py
+++
@@ -1,9 +0,0 @@
-from odoo import models, fields
-
-class SscTestSimple(models.Model):
-    _name = "ssc.test.simple"
-    _description = "Simple Test Menu"
-
-    name = fields.Char(string="Name", required=True)  # إضافة حقل name
-    date_field = fields.Date(string="Date")
-    is_checked = fields.Boolean(string="Checked")
