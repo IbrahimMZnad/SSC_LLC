60605d4a692126b26d597ce70c53ee22de6f0a04
Ibrahim Elzenad <id7sp22@gmail.com>
2025-10-15 16:54:20 +0400

    Add new changes

---
+++ addons/worksite_analytics/__manifest__.py
@@ -0,0 +1,19 @@
+{
+    "name": "WorkSite Analytics - Store Reports",
+    "version": "1.0",
+    "summary": "Generates professional store reports with monthly tabs for Consumed Materials",
+    "description": """
+        Generates reports for each store with monthly tabs (January 2024 onwards),
+        showing Materials Consumed data from x_transaction.
+    """,
+    "author": "Ibrahim Elzenad",
+    "depends": ["base", "x_inventory_stores_pro", "x_transaction"],
+    "data": [
+        "views/store_report_views.xml",
+        "reports/store_report_templates.xml",
+        "data/cron_generate_store_reports.xml"
+    ],
+    "installable": True,
+    "application": True,
+    "auto_install": False,
+}
