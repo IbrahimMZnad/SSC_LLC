f5d6039bed678f7ec2d13ac770a317271ddbb10f
Ibrahim Elzenad <id7sp22@gmail.com>
2025-10-16 14:31:39 +0400

    del

--- addons/store_monthly_report/__manifest__.py
+++ addons/store_monthly_report/__manifest__.py
@@ -6,7 +6,7 @@
     'summary': 'Generate monthly reports for stores from consumed materials',
     'description': 'This module generates monthly store reports using consumed materials.',
     'author': 'Ibrahim Elzenad',
-    'depends': ['base', 'x_inventory_stores_pro', 'x_transaction', 'x_projects_list'],
+    'depends': ['base'],
     'data': [
         'views/store_monthly_report_views.xml',
         # لو عندك أي security أو data files أضفهم هنا
