c72f939cfddd4860f91cd0a0783559bb7216ad50
Ibrahim Elzenad <id7sp22@gmail.com>
2025-10-29 15:18:41 +0400

    lara

--- addons/consumtion_report/__manifest__.py
+++ addons/consumtion_report/__manifest__.py
@@ -1,16 +1,14 @@
+# -*- coding: utf-8 -*-
 {
     'name': 'Consumption Report',
     'version': '1.0',
-    'category': 'Custom',
-    'summary': 'Report for consumed materials',
-    'description': 'Module to generate report of consumed materials based on date, project, company, and items.',
-    'author': 'Ibrahim Alznad',
+    'author': 'Ibrahim M. Elznad',
+    'category': 'Warehouse',
+    'summary': 'Generate report of consumed materials by project, company, date and item',
     'depends': ['base'],
     'data': [
         'views/consumtion_report_views.xml',
-        'views/consumtion_report_menu.xml',
     ],
-    'installable': True,
     'application': True,
-    'auto_install': False,
+    'installable': True,
 }
