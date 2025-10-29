4c1dcf0b8274def51f4392e5473a5145d45a428e
Ibrahim Elzenad <id7sp22@gmail.com>
2025-10-29 12:14:57 +0400

    ll

--- addons/consumtion_material/__manifest__.py
+++ addons/consumtion_material/__manifest__.py
@@ -5,7 +5,7 @@
     'category': "Custom",
     'summary': "Report of all consumed materials",
     'description': "Module to generate Consumption Material report based on filters",
-    'depends': ['base', 'x_transaction', 'x_projects_list', 'x_all_items_list'],
+    'depends': ['base'],
     'data': [
         'views/consumtion_material_views.xml',
     ],
