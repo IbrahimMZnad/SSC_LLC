cd4db3c815f2802634059d804ba5515a37e5df6a
Ibrahim Elzenad <id7sp22@gmail.com>
2025-10-29 13:05:54 +0400

    add

--- addons/consumption_report_new/__manifest__.py
+++ addons/consumption_report_new/__manifest__.py
@@ -1,19 +1,15 @@
 # -*- coding: utf-8 -*-
 {
-    'name': 'Consumption Material New',
+    'name': 'Consumption Material Report',
     'version': '1.0',
+    'author': 'Ibrahim M. Elznad',
     'category': 'Reports',
-    'summary': 'Consumption Material Report Module New',
-    'description': """
-Generate and filter consumed materials based on company, project, date range, and items.
-""",
-    'author': 'Ibrahim Alznad',
+    'summary': 'Generate monthly and daily consumption material reports by project and company',
     'depends': ['base'],
     'data': [
-        'views/consumption_report_new_views.xml',
+        'views/consumption_material_report_views.xml',
     ],
-    'installable': True,
     'application': True,
-    'auto_install': False,
+    'installable': True,
     'license': 'LGPL-3',
 }
