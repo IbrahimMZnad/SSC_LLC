f4dbfdabaada866824cdf4d1a90bb90c3ffd3aec
Ibrahim Elzenad <id7sp22@gmail.com>
2025-10-30 10:48:55 +0400

    lara9

--- addons/project_material_consumption/__manifest__.py
+++ addons/project_material_consumption/__manifest__.py
@@ -1,15 +1,11 @@
 {
-    "name": "Project Material Consumption",
+    'name': 'Project Material Consumption',
     'version': '18.0.1.0',
-    "summary": "Track materials needed, consumed and ordered per project",
-    "category": "Operations",
-    "author": "Ibrahim",
-    "website": "",
-    "license": "AGPL-3",
-    "depends": ["base", "purchase"],
-    "data": [
-        "views/project_material_consumption_views.xml",
+    'author': 'Ibrahim M. Alznad',
+    'depends': ['base'],
+    'data': [
+        'views/project_material_consumption_views.xml',
     ],
-    "installable": True,
-    "application": False,
+    'application': True,
+    'installable': True,
 }
