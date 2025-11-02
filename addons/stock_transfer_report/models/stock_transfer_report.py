dfb803e53047b7f7ef4aaf00178b350cf0e5f89d
Ibrahim Elzenad <id7sp22@gmail.com>
2025-11-02 13:30:36 +0400

    lara

--- addons/stock_transfer_report/models/stock_transfer_report.py
+++ addons/stock_transfer_report/models/stock_transfer_report.py
@@ -13,6 +13,32 @@ class StockTransferReport(models.Model):
     outgoing_lines = fields.One2many('stock.transfer.line', 'transfer_id', string="Outgoing Lines")
     incoming_lines = fields.One2many('stock.transfer.line', 'transfer_id', string="Incoming Lines")
 
+    def fill_lines_from_transactions(self):
+        Transaction = self.env['x_transaction']
+        for rec in self:
+            transactions = Transaction.search([
+                ('x_studio_type_of_transaction', '=', 'Transfer'),
+                ('x_studio_selection_field_64t_1ipgtrlhm', '=', 'status2'),
+                ('x_studio_from_store', '=', rec.from_store.id),
+                ('x_studio_store', '=', rec.to_store.id),
+            ])
+            for tx in transactions:
+                for line in tx.x_studio_transfering_details:
+                    # Outgoing Line
+                    self.env['stock.transfer.line'].create({
+                        'transfer_id': rec.id,
+                        'description': 'to ' + rec.to_store.name,
+                        'item': line.x_studio_item.id,
+                        'date': tx.x_studio_date_2,
+                    })
+                    # Incoming Line
+                    self.env['stock.transfer.line'].create({
+                        'transfer_id': rec.id,
+                        'description': 'from ' + rec.to_store.name,
+                        'item': line.x_studio_item.id,
+                        'date': tx.x_studio_date_2,
+                    })
+
 
 class StockTransferLine(models.Model):
     _name = 'stock.transfer.line'
@@ -24,3 +50,4 @@ class StockTransferLine(models.Model):
     item_serial_no = fields.Char(string="Item Serial No.", related='item.x_studio_item_serial_no', store=True)
     unit = fields.Char(string="Unit", related='item.x_studio_unit', store=True)
     type_of_material = fields.Many2one('x_type_of_material', string="Type Of Material", related='item.x_studio_type_of_material', store=True)
+    date = fields.Datetime(string="Date")
