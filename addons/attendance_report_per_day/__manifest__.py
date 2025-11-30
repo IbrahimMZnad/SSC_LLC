04c454387188d77e2b08b306eacd79f06c4c014d
Ibrahim Elzenad <id7sp22@gmail.com>
2025-11-30 12:12:40 +0400

    add a new module

---
+++ addons/attendance_report_per_day/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    'name': 'Attendance Report Per Day',
+    'version': '1.0',
+    'category': 'HR',
+    'summary': 'Daily Attendance Summary per Project',
+    'author': 'Ibrahim',
+    'depends': ['base'],
+    'data': [
+        'views/menu.xml',
+        'views/attendance_report_views.xml',
+    ],
+    'installable': True,
+    'application': True,
+}
---
+++ addons/attendance_report_per_day/models/__init__.py
@@ -0,0 +1,2 @@
+from . import attendance_report_per_day
+from . import attendance_line
