c2a2224050413dbe7ef6a725e4fd119e5e272d2c
Ibrahim Elzenad <id7sp22@gmail.com>
2025-11-30 13:03:59 +0400

    af

--- addons/attendance_report_per_day/models/attendance_lines.py
+++ addons/attendance_report_per_day/models/attendance_lines.py
@@ -1,15 +1,18 @@
-from odoo import models, fields, api
-
 class AttendanceReportLine(models.Model):
     _name = 'attendance.report.line'
     _description = 'Attendance Report Line'
 
+    external_id = fields.Many2one('attendance.report', string='Report')
     employee_id = fields.Many2one('x_employeeslist', string='Employee Name')
-    employee_att_id = fields.Char(string='Employee Att ID', compute='_compute_att_id')
+    employee_att_id = fields.Char(
+        string='Employee Att ID', 
+        compute='_compute_employee_att_id',
+        store=True
+    )
     first_punch = fields.Datetime(string='First Punch')
     last_punch = fields.Datetime(string='Last Punch')
 
     @api.depends('employee_id')
-    def _compute_att_id(self):
+    def _compute_employee_att_id(self):
         for line in self:
-            line.employee_att_id = line.employee_id.x_studio_attendance_id if line.employee_id else ''
+            line.employee_att_id = line.employee_id.x_studio_attendance_id or ''
